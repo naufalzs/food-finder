787ce84446048411d428c9ee0aec6d4f7bbb962c
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-10-06 14:06:05 +0700

    feat: setup custom font and normalize css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Home from "./pages/Home";
 
 export default function App() {
-  return <Home />;
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["Raleway", "sans-serif"].join(","),
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Home />
+      </CssBaseline>
+    </ThemeProvider>
+  );
 }
