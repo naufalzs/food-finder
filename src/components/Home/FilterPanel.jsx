7c226ba058541adc2fcc86fa59342c8436554bf5
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-10-13 08:59:56 +0700

    feat: create category filter

--- src/components/Home/FilterPanel.jsx
+++ src/components/Home/FilterPanel.jsx
@@ -1,20 +1,19 @@
 import { Box, Typography } from "@mui/material";
 import { FilterListToggle } from "../common/FilterListToggle";
+import { categoryList } from "@/constants";
 
-export default function FilterPanel() {
+export default function FilterPanel({selectedCategory, selectCategory}) {
   return (
     <Box p={2}>
       <Box mb={4}>
         <Typography variant="h6" fontWeight={600} mb={1}>
           Category
         </Typography>
-        <FilterListToggle />
-      </Box>
-      <Box mb={4}>
-        <Typography variant="h6" fontWeight={600} mb={1}>
-          Category
-        </Typography>
-        <FilterListToggle />
+        <FilterListToggle
+          options={categoryList}
+          value={selectedCategory}
+          selectToggle={selectCategory}
+        />
       </Box>
     </Box>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,46 @@
+import { useEffect, useState } from "react";
 import { Box, Divider, Grid } from "@mui/material";
+
 import FilterPanel from "@/components/Home/FilterPanel";
 import ResultList from "@/components/Home/ResultList";
 import SearchBar from "@/components/Home/SearchBar";
 
+import { categoryList, dataList } from "@/constants";
+
 export default function Home() {
+  const allCategory = categoryList.map((item) => item.value);
+
+  const [selectedCategory, setSelectedCategory] = useState(allCategory);
+  const [list, setList] = useState(dataList);
+
+  const handleSelectCategory = (event, value) => setSelectedCategory(value);
+
+  const applyFilter = () => {
+    let updatedList = dataList;
+
+    if (selectedCategory.length > 0) {
+      updatedList = updatedList.filter((item) => {
+        return selectedCategory.includes(item.category);
+      });
+    }
+
+    setList(updatedList);
+  };
+
+  useEffect(() => {
+    applyFilter();
+  }, [selectedCategory]);
+
   return (
     <Box>
       <SearchBar />
       <Divider />
       <Grid container>
         <Grid item sx={{ width: 300 }}>
-          <FilterPanel />
+          <FilterPanel
+            selectedCategory={selectedCategory}
+            selectCategory={handleSelectCategory}
+          />
         </Grid>
         <Grid item xs>
           <ResultList />
