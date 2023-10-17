a78e24f6a0ea97c05a8c6a6aabaf85d18099d04e
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-10-17 11:01:26 +0700

    feat(responsive): setup grid on smaller viewport

--- src/components/Home/ResultList.jsx
+++ src/components/Home/ResultList.jsx
@@ -1,11 +1,14 @@
-import { Grid } from "@mui/material";
+import { Grid, useMediaQuery } from "@mui/material";
 import { CardItem } from "./CardItem";
 
 export default function ResultList({ list }) {
+  const isTablet = useMediaQuery((theme) => theme.breakpoints.down("md"));
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
   return (
     <Grid container spacing={3} p={2} pl={3} pr={2.5}>
       {list.map((item) => (
-        <Grid item xs={4} key={item.id}>
+        <Grid item xs={isTablet ? (isMobile ? 12 : 6) : 4} key={item.id}>
           <CardItem
             title={item.title}
             imgThumb={item.coverSrc}
